c473cd0ccd0f69b7188a07395dbf6a7a6241d9cc
wmt-harshs <wmt-harshs@users.noreply.github.com>
2022-03-21 07:53:03 +0000

    deploy: 20a14c62bbe9b606f5c6c847a0efe54bc2fa90dd

---
+++ precache-manifest.e25d50e7c10187c35bafe73812956705.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9388ce5940cbc55b77c39d3b8941e2e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dfcaea4e09a40b64765",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9dfcaea4e09a40b64765",
+    "url": "/static/js/2.69fef3bc.chunk.js"
+  },
+  {
+    "revision": "4ad2c970c2d6d722e50f",
+    "url": "/static/js/main.27c2633d.chunk.js"
+  },
+  {
+    "revision": "71b89c0126599940f074",
+    "url": "/static/js/runtime-main.204403d4.js"
+  }
+]);
